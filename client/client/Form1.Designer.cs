af7cd59b9262e77449be8710fed4d8ffbeeb15e5
BogachanA <bogacbugra@gmail.com>
2023-01-17 21:02:03 +0300

    final version

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.textBox_location = new System.Windows.Forms.TextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.button_choosefile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(67, 50);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(88, 20);
+            this.textBox_ip.TabIndex = 2;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(67, 79);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(88, 20);
+            this.textBox_port.TabIndex = 3;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(67, 138);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(70, 22);
+            this.button_connect.TabIndex = 4;
+            this.button_connect.Text = "connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(178, 49);
+            this.logs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(261, 279);
+            this.logs.TabIndex = 5;
+            this.logs.Text = "";
+            // 
+            // textBox_location
+            // 
+            this.textBox_location.Enabled = false;
+            this.textBox_location.Location = new System.Drawing.Point(6, 232);
+            this.textBox_location.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_location.Name = "textBox_location";
+            this.textBox_location.Size = new System.Drawing.Size(150, 20);
+            this.textBox_location.TabIndex = 6;
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(89, 257);
+            this.button_send.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(67, 19);
+            this.button_send.TabIndex = 8;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // button_choosefile
+            // 
+            this.button_choosefile.Enabled = false;
+            this.button_choosefile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_choosefile.Location = new System.Drawing.Point(6, 257);
+            this.button_choosefile.Name = "button_choosefile";
+            this.button_choosefile.Size = new System.Drawing.Size(78, 19);
+            this.button_choosefile.TabIndex = 9;
+            this.button_choosefile.Text = "Choose File";
+            this.button_choosefile.UseVisualStyleBackColor = true;
+            this.button_choosefile.Click += new System.EventHandler(this.button_choosefile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Username:";
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(67, 105);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(87, 20);
+            this.textBox_username.TabIndex = 11;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 358);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_choosefile);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.textBox_location);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.TextBox textBox_location;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Button button_choosefile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ client/client/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        bool terminating = false;
+        bool connected = false;
+        Socket clientSocket;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            InitializeComponent();
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox_ip.Text;
+            string port = textBox_port.Text;
+            string username = textBox_username.Text;
+
+            int portNum;
+            if(Int32.TryParse(textBox_port.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    button_connect.Enabled = false;
+                    textBox_location.Enabled = true;
+                    button_send.Enabled = true;
+                    button_choosefile.Enabled = true;
+                    connected = true;
+                    logs.AppendText("Connected to the server!\n");
+
+                    Byte[] buffer2 = new Byte[64];
+                    buffer2 = Encoding.Default.GetBytes(username);
+                    clientSocket.Send(buffer2);
+
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start();
+
+                }
+                catch
+                {
+                    logs.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                logs.AppendText("Check the port\n");
+            }
+
+        }
+
+        private void Receive()
+        {
+            while(connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+                    if (incomingMessage.Length != 0)
+                        logs.AppendText("Server: " + incomingMessage + "\n");
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs.AppendText("The server has disconnected\n");
+                        button_connect.Enabled = true;
+                        textBox_location.Enabled = false;
+                        button_send.Enabled = false;
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            string filename = textBox_location.Text.Substring(textBox_location.Text.LastIndexOf('\\')+1);
+               /* Byte[] buffer = new Byte[64];
+                buffer = Encoding.Default.GetBytes(message);
+                clientSocket.Send(buffer); */
+            try
+            {
+                string text = File.ReadAllText(textBox_location.Text);
+                string text_final = filename + "~" + text;
+                size = text.Length;
+                Byte[] buffer3 = new Byte[size];
+                buffer3 = Encoding.Default.GetBytes(text_final);
+                clientSocket.Send(buffer3);
+                //logs.AppendText("valla yolladım.\n");
+                
+
+            }
+            catch
+            {
+            }
+
+        }
+
+        private void button_choosefile_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            textBox_location.Clear();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                textBox_location.AppendText(file);
+                
+            }
+        }
+    }
+}
